63b6e5f04f3bfb71b07b73a7fe63919857d02cdc
jacobreesgit <jacobrees@me.com>
2025-06-27 16:02:33 +0100

    Initial Commit

---
+++ Virtual Pet Blob/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Virtual Pet Blob
+//
+//  Created by Jacob Rees on 27/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Virtual Pet Blob/Virtual_Pet_BlobApp.swift
@@ -0,0 +1,17 @@
+//
+//  Virtual_Pet_BlobApp.swift
+//  Virtual Pet Blob
+//
+//  Created by Jacob Rees on 27/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Virtual_Pet_BlobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
